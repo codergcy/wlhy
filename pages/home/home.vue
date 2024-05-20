6fa7bbfffb9ba9131fe206e7c56e3e256b0947de
codergcy <codergcy7@gmail.com>
2024-05-20 14:05:15 +0800

    feat:'修改我的页面'

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,23 +1,24 @@
 <template name="home">
 	<view>
 		<scroll-view>
+			<u-navbar  title="我的"></u-navbar>
 			<!-- 轮播 -->
-			<swiper class="screen-swiper square-dot"  :indicator-dots="true" :circular="true"
+			<!-- <swiper class="screen-swiper square-dot"  :indicator-dots="true" :circular="true"
 			 :autoplay="true" interval="5000" duration="500" :style="[{animation: 'show 0.2s 1'}]">
 				<swiper-item v-for="(item,index) in swiperList" :key="index">
 					<image :src="item.url" mode="aspectFill" v-if="item.type=='image'"></image>
 					<video :src="item.url" autoplay loop muted :show-play-btn="false" :controls="false" objectFit="cover" v-if="item.type=='video'"></video>
 				</swiper-item>
-			</swiper>
+			</swiper> -->
 			
-			<!-- 常用服务 -->
+			<!-- 会员中心 -->
 			<view class="cu-bar bg-white solid-bottom" :style="[{animation: 'show 0.5s 1'}]">
 				<view class="action">
-					<text class='cuIcon-title text-blue'></text>常用服务
+					<text class='cuIcon-title text-blue'></text>会员中心
 				</view>
 			</view>
 				
-			<view class="cu-list grid col-4 text-sm">
+			<view class="cu-list grid col-2 text-sm">
 				<view class="cu-item animation-slide-bottom" :style="[{animationDelay: (index + 1)*0.05 + 's'}]" v-for="(item,index) in usList" :key="index" @tap="goPage(item.page)">
 					<view class="padding text-center">
 						<image :src="item.icon" style="width:28px;height:28px;">
@@ -30,13 +31,13 @@
 				</view>
 			</view>
 				
-			<!-- 其他服务 -->
+			<!-- 运力中心 -->
 			<view class="cu-bar bg-white solid-bottom margin-top"  :style="[{animation: 'show 0.6s 1'}]">
 				<view class="action">
-					 <text class='cuIcon-title text-yellow'></text>其他服务
+					 <text class='cuIcon-title text-yellow'></text>运力中心
 				</view>
 			</view>
-			<view class="cu-list grid col-4 text-sm">
+			<view class="cu-list grid col-2 text-sm">
 				<view class="cu-item animation-slide-bottom" :style="[{animationDelay: (index + 1)*0.1 + 's'}]" v-for="(item,index) in osList" :key="index" @tap="goPage(item.page)">
 					<view class="padding text-center">
 						<image :src="item.icon" style="width:28px;height:28px;"/>
@@ -57,15 +58,15 @@
 		props:{
 			cur:String,
 		},
-		watch: {
-			cur: {
-				immediate: true,
-				handler:function(val,oldVal){
-					console.log('cur',val,oldVal)
-				    this.initMenu()
-				},
-			},
-		},
+		// watch: {
+		// 	cur: {
+		// 		immediate: true,
+		// 		handler:function(val,oldVal){
+		// 			console.log('cur',val,oldVal)
+		// 		    this.initMenu()
+		// 		},
+		// 	},
+		// },
 		data() {
 			return {
 			 swiperList: [
@@ -87,12 +88,12 @@
 			}
 		},
 		methods: {
-			initMenu(){
-				console.log("-----------home------------")
-			    this.onSocketOpen()
-			    this.onSocketReceive()
-			    this.loadCount(0);
-			},
+			// initMenu(){
+			// 	console.log("-----------home------------")
+			//     this.onSocketOpen()
+			//     this.onSocketReceive()
+			//     this.loadCount(0);
+			// },
 			goPage(page){
 				if(!page){
 					return false;
@@ -103,26 +104,26 @@
 				this.dot[page]=false
 				this.$Router.push({name: page})
 			},
-			// 启动webSocket
-			onSocketOpen() {
-				socket.init('websocket');
-			},
-			onSocketReceive() {
-				var _this=this
-				socket.acceptMessage = function(res){
-					// console.log("页面收到的消息", res);
-					if(res.cmd == "topic"){
-					  //系统通知
-					  _this.loadCount('1')
-					}else if(res.cmd == "user"){
-					  //用户消息
-					  _this.loadCount('2')
-					} else if(res.cmd == 'email'){
-					 //邮件消息
-					  _this.loadEmailCount()
-					}
-				}
-			},
+			// // 启动webSocket
+			// onSocketOpen() {
+			// 	socket.init('websocket');
+			// },
+			// onSocketReceive() {
+			// 	var _this=this
+			// 	socket.acceptMessage = function(res){
+			// 		// console.log("页面收到的消息", res);
+			// 		if(res.cmd == "topic"){
+			// 		  //系统通知
+			// 		  _this.loadCount('1')
+			// 		}else if(res.cmd == "user"){
+			// 		  //用户消息
+			// 		  _this.loadCount('2')
+			// 		} else if(res.cmd == 'email'){
+			// 		 //邮件消息
+			// 		  _this.loadEmailCount()
+			// 		}
+			// 	}
+			// },
 			loadCount(flag){
 				console.log("loadCount::flag",flag)
 				let url = '/sys/annountCement/listByUser';
